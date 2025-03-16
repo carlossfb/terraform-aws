02fc5c6c75e09e47d941b29737e9101252f664a5
Carlos Eduardo <72090014+carlossfb@users.noreply.github.com>
2025-03-16 20:27:58 -0300

    Update outputs.tf

--- aws_iam/modules/aws_role/outputs.tf
+++ aws_iam/modules/aws_role/outputs.tf
@@ -2,3 +2,8 @@ output "iam_role_arn" {
   value = aws_iam_role.role.arn
   description = "O ARN da role IAM criada"
 }
+
+output "iam_role_name" {
+  value = aws_iam_role.role.name
+  description = "O name da role IAM criada"
+}
